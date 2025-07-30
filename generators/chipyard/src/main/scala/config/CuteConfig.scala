f33bf9944a4b38b7dae2772028bc8e65b1aa2dad
Yuan Bin <1024364354@qq.com>
2025-07-30 20:14:44 +0800

    [feature] add verify config

--- generators/chipyard/src/main/scala/config/CuteConfig.scala
+++ generators/chipyard/src/main/scala/config/CuteConfig.scala
@@ -3,11 +3,22 @@ package chipyard
 import org.chipsalliance.cde.config.{Config}
 import saturn.common.{VectorParams}
 import freechips.rocketchip.subsystem.InTile
-import cute.CuteParams
+import cute._
 // ---------------------
 // BOOM V3 Configs
 // Performant, stable baseline
 // ---------------------
+
+class VerifyL2DramPerformenceTest1CUTEM256Config extends Config(
+  new cute.WithCuteCoustomParams(CoustomCuteParam = CuteParams.dram_L2_8Tops_PerformanceTestParams.copy(Debug = CuteDebugParams.CMLDebugEnable)) ++
+  new cute.WithCUTE(Seq(0)) ++
+  new chipyard.config.WithSystemBusWidth(256) ++
+  new boom.v3.common.WithNSmallBooms(1) ++                          // small boom config
+  new freechips.rocketchip.subsystem.WithoutTLMonitors ++
+  new freechips.rocketchip.subsystem.WithNBitMemoryBus(dataBits = 256) ++ //设置访存总线的位宽
+  new freechips.rocketchip.subsystem.WithNBanks(4) ++
+  new freechips.rocketchip.subsystem.WithInclusiveCache(capacityKB=512,outerLatencyCycles=100) ++
+  new chipyard.config.AbstractConfig)
 
 class L2DramPerformenceTest1CUTEM64Config extends Config(
   new cute.WithCuteCoustomParams(CoustomCuteParam = CuteParams.dram_L2_8Tops_PerformanceTestParams) ++
